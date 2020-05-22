9b7a3bf088e284d7856df1b2c855567a9f09e5c7
Binh Tran <thanhbinh.tran93@gmail.com>
2020-05-22 15:07:58 +0300

    feat: Adding Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,66 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Avatar, Button } from '@material-ui/core';
+import { makeStyles, Avatar, Button, AppBar, Toolbar, Typography, Link } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  navContainer: {
+    display: 'flex',
+    flex: 1,
+    '& > * + *': {
+      marginLeft: theme.spacing(2),
+    },
+  },
+  navSignin: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(0, 1, 0, 1),
+  },
+}));
 
 const Nav = ({ user }) => {
+  const classes = useStyles();
+
+  const handleLogout = () => {};
+
   return (
-    <nav className="nav">
-      <div className="nav-container">
-        <NavLink to="/" exact activeClassName="active">
-          Home
-        </NavLink>
-        <NavLink to="/add" activeClassName="active">
-          New Question
-        </NavLink>
-        <NavLink to="/leaderboard" activeClassName="active">
-          Leader Board
-        </NavLink>
-      </div>
-      {user && (
-        <div className="nav-signin">
-          <span>Hello {user.name}</span>
-          <Avatar alt={user.name} src={user.avatarURL} />
-          <Button>Logout</Button>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <div className={classes.navContainer}>
+          <Link color="inherit" component={NavLink} to="/" exact activeClassName="MuiLink-underlineAlways">
+            Home
+          </Link>
+          <Link color="inherit" component={NavLink} to="/add" activeClassName="MuiLink-underlineAlways">
+            New Question
+          </Link>
+          <Link color="inherit" component={NavLink} to="/leaderboard" activeClassName="MuiLink-underlineAlways">
+            Leader Board
+          </Link>
         </div>
-      )}
-    </nav>
+        {user && (
+          <div className={classes.navSignin}>
+            <Typography>Hello {user.name}</Typography>
+            <Avatar className={classes.avatar} alt={user.name} src={user.avatarURL} />
+            <Button variant="outlined" color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
 
